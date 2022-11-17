3679276d69e1f1747cf70e312b934fd1c9a3df41
Harshil Sethi <h.sethi@ceccli.com>
2022-11-17 16:53:18 +0100

    Add paginate in Scenario

--- api2/getScenario.js
+++ api2/getScenario.js
@@ -10,16 +10,20 @@ router.get('/getScenario', (req, res) => {
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
     res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
-    const id = Number(req.query.id);
-    const typeSupport = req.query.typeSupport;
-    console.log('router.get ~ id', id);
+    // const id = Number(req.query.id);
+    // const typeSupport = req.query.typeSupport;
+    const pageIndex = req.query.pageIndex ?? 0;
+    const pageSize = req.query.pageSize ?? 100;
+    const request = `FROM Scenario
+    INNER JOIN DeploiementScenario ON DeploiementScenario.AttScenario = Scenario.CleIdt
+    INNER JOIN ScenarioDeploye ON ScenarioDeploye.AttDeploiementScenario = DeploiementScenario.CleIdt`;
 
-    const sql = `SELECT Scenario.NomFichier, Scenario.Version, DeploiementScenario.DateApplication, ScenarioDeploye.DateChargementFin
-        FROM Scenario
-        INNER JOIN DeploiementScenario ON DeploiementScenario.AttScenario = Scenario.CleIdt
-        INNER JOIN ScenarioDeploye ON ScenarioDeploye.AttDeploiementScenario = DeploiementScenario.CleIdt
-        WHERE DeploiementScenario.AttSupport = ${id} AND TypeSupport = '${typeSupport}' ${typeSupport === 'VEH' ? 'AND ScenarioDeploye.DateChargementFin IS NOT NULL' : ''}
-        ORDER BY ScenarioDeploye.DateChargementFin DESC LIMIT 0, 1;`
+    const sql = `SELECT Scenario.NomFichier, Scenario.Version, DeploiementScenario.DateApplication, ScenarioDeploye.DateChargementFin, DeploiementScenario.AttSupport, \
+        ( SELECT COUNT(*) as TotalCount ${request} ) as TotalCount \
+        ${request} \
+        ORDER BY ScenarioDeploye.DateChargementFin DESC
+        LIMIT ${pageSize} OFFSET ${pageIndex * pageSize}`;
+        // WHERE DeploiementScenario.AttSupport = ${id} AND TypeSupport = '${typeSupport}' ${typeSupport === 'VEH' ? 'AND ScenarioDeploye.DateChargementFin IS NOT NULL' : ''}
     console.log('con.query ~ sql', sql);
     con.query(sql, (err, results) => {
         if (err) throw err

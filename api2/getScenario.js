52e9648cb67f8cb49812e6c71e989710ed35f57a
Harshil <harshil@hotmail.fr>
2022-11-18 14:33:58 +0100

    Add fitler on scenario

--- api2/getScenario.js
+++ api2/getScenario.js
@@ -12,15 +12,32 @@ router.get('/getScenario', (req, res) => {
 
     // const id = Number(req.query.id);
     // const typeSupport = req.query.typeSupport;
+    const vehicleId = Number(req.query.vehicleId);
+    const date = req.query.date;
     const pageIndex = req.query.pageIndex ?? 0;
     const pageSize = req.query.pageSize ?? 100;
+
+    const whereClauses = [
+        vehicleId ? `AttSupport = ${vehicleId}` : undefined,
+        date ? `DATE(DateHeure) = '${moment(date).format('YYYY-MM-DD')}'` : undefined,
+    ].filter(c => c);
+
+    let filter = '';
+    if (whereClauses.length > 1) {
+        filter = whereClauses.reduce((a, b, index) => {
+            return (`${index === 1 ? 'WHERE' : ''} ${a} AND ${b}`);
+        });
+    } else if (whereClauses.length === 1) {
+        filter = `WHERE ${whereClauses[0]}`;
+    }
+
     const request = `FROM Scenario
     INNER JOIN DeploiementScenario ON DeploiementScenario.AttScenario = Scenario.CleIdt
     INNER JOIN ScenarioDeploye ON ScenarioDeploye.AttDeploiementScenario = DeploiementScenario.CleIdt`;
 
-    const sql = `SELECT Scenario.NomFichier, Scenario.Version, DeploiementScenario.DateApplication, ScenarioDeploye.DateChargementFin, DeploiementScenario.AttSupport, \
-        ( SELECT COUNT(*) as TotalCount ${request} ) as TotalCount \
-        ${request} \
+    const sql = `SELECT Scenario.NomFichier, Scenario.Version, DeploiementScenario.DateApplication, ScenarioDeploye.DateChargementFin, DeploiementScenario.AttSupport,
+        ( SELECT COUNT(*) as TotalCount ${request} ${filter} ) as TotalCount
+        ${request} ${filter}
         ORDER BY ScenarioDeploye.DateChargementFin DESC
         LIMIT ${pageSize} OFFSET ${pageIndex * pageSize}`;
         // WHERE DeploiementScenario.AttSupport = ${id} AND TypeSupport = '${typeSupport}' ${typeSupport === 'VEH' ? 'AND ScenarioDeploye.DateChargementFin IS NOT NULL' : ''}
